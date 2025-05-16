2062a207479a331f58f0677c84490eee946f14f7
swim233 <swim853279614@163.com>
2025-05-16 19:46:21 +0800

    modify

--- init.sh
+++ init.sh
@@ -4,46 +4,45 @@ sleep 1
 sudo apt update && sudo apt upgrade -y
 sudo apt install zsh neovim python3 btop lolcat vim git wget curl neofetch lolcat -y
 if [[ $? -ne 0 ]]; then
-    echo -e "\033[1;31m常用软件安装失败，请检查网络或源配置。\033[0m"
-    exit 1
+  echo -e "\033[1;31m常用软件安装失败，请检查网络或源配置。\033[0m"
+  exit 1
 else
-    echo -e "\033[1;32m常用软件安装成功！\033[0m"
+  echo -e "\033[1;32m常用软件安装成功！\033[0m"
 fi
 echo -e "\033[1;34m是否安装lsd(y/N)\033[0m"
 read -r install_lsd
 if [[ "$install_lsd" == "y" || "$install_lsd" == "Y" ]]; then
-    echo -e "\033[1;34m正在安装lsd\n\033[0m"
-    sudo apt install lsd
-    if [[ $? -ne 0 ]]; then
-        echo -e "\033[1;31m lsd 安装失败，请检查网络或源配置。\033[0m"
-    else
-        echo -e "\033[1;32m lsd安装成功！\033[0m"
-    fi
+  echo -e "\033[1;34m正在安装lsd\n\033[0m"
+  sudo apt install lsd
+  if [[ $? -ne 0 ]]; then
+    echo -e "\033[1;31m lsd 安装失败，请检查网络或源配置。\033[0m"
+  else
+    echo -e "\033[1;32m lsd安装成功！\033[0m"
+  fi
 else
-    echo "跳过lsd安装"
+  echo "跳过lsd安装"
 fi
 
 echo -e "\033[1;34m是否安装omz和p10k以及部分插件(y/N)\033[0m"
 read -r install_omz
 if [[ "$install_omz" == "y" || "$install_omz" == "Y" ]]; then
-    echo -e "\033[1;34m正在安装omz和p10k以及部分插件\033[0m"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-    
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    
-    sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
-    
-    sed -i 's/(git)/(git extract zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
-    
-    
-    echo -e "\033[1;32m omz相关安装成功！\033[0m"
-    
+  echo -e "\033[1;34m正在安装omz和p10k以及部分插件\033[0m"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+  sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
+
+  sed -i 's/(git)/(git extract zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+
+  echo -e "\033[1;32m omz相关安装成功！\033[0m"
+
 else
-    echo -e "\033[1;32m 跳过omz安装\033[0m"
+  echo -e "\033[1;32m 跳过omz安装\033[0m"
 fi
 
 #TODO 修改选项
@@ -51,16 +50,18 @@ fi
 echo -e "\033[1;34m是否添加lsd的alias (y/N)\033[0m"
 read -r alias_lsd
 if [[ "$alias_lsd" == "y" || "$alias_lsd" == "Y" ]]; then
-    echo 'alias ls=lsd' >> ~/.zshrc
+  echo 'alias ls=lsd' >>~/.zshrc
 fi
 echo -e "\033[1;34m跳过添加lsd的alias\n\033[0m"
 
 echo -e "\033[1;34m是否添加登录时的ASCII (y/N)\033[0m"
 read -r login_ascii
 if [[ "$login_ascii" == "y" || "$login_ascii" == "Y" ]]; then
-
-mkdir -p ~/.scripts/
-echo "MOTD_CONTENT=$(cat << 'EOF'
+  mkdir -p ~/.scripts/
+  echo "
+  #!/bin/bash
+  MOTD_CONTENT=$(
+    cat <<'EOF'
 
      ██╗██╗   ██╗███████╗████████╗    ██████╗ ███╗   ███╗          ██████╗ ███████╗        ██╗
      ██║██║   ██║██╔════╝╚══██╔══╝    ██╔══██╗████╗ ████║          ██╔══██╗██╔════╝       ██╔╝▄ ██╗▄
@@ -71,20 +72,18 @@ echo "MOTD_CONTENT=$(cat << 'EOF'
 
 
 EOF
-)
+  )
 
-# 使用lolcat渲染（如果安装）
 if command -v lolcat &> /dev/null; then
     echo -e "$MOTD_CONTENT" | lolcat
 else
     echo -e "$MOTD_CONTENT"
 fi
-" > ~/.scripts/motd.sh
-chmod +x ~/.scripts/motd.sh
-echo "/home/$USER/.scripts/motd.sh" >> ~/.zhsrc
+" >~/.scripts/motd.sh
+  chmod +x ~/.scripts/motd.sh
+  echo "/home/$USER/.scripts/motd.sh" >>~/.zhsrc
 
 fi
-    echo -e "\033[1;32m 跳过ASCII添加\033[0m"
+echo -e "\033[1;32m跳过ASCII添加\033[0m"
 
 echo -e "\033[1;34m所有步骤执行完成 请手动运行"source ~/.zshrc"\n\033[0m"
-
